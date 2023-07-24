c933f4c23c0d876a9ac7abbbb85a61a1601918cb
Kye <kye@apacmediasolutions.com>
2023-07-24 19:10:13 -0400

    clean up

--- benchmark.py
+++ benchmark.py
@@ -1,11 +1,9 @@
-import timeit
 import time
 import torch
-import numpy as np
-
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
 from softmax_one import softmax1
+import numpy as np
 
 def benchmark(func, x, dim):
     start = time.time()
@@ -24,18 +22,18 @@ times_softmax1 = []
 # Run the benchmark
 for size in sizes:
     x = torch.rand(size)
-    time_softmax = benchmark(F.softmax, x)
-    time_softmax1 = benchmark(softmax1, x, dim=0)
+    time_softmax = benchmark(F.softmax, x, dim=-1)
+    time_softmax1 = benchmark(softmax1, x, dim=-1)
     
     times_softmax.append(time_softmax)
     times_softmax1.append(time_softmax1)
 
 # Plot the results
 plt.figure(figsize=(10, 6))
-plt.plot(sizes, times_softmax, label='F.softmax')
-plt.plot(sizes, times_softmax1, label='softmax1')
+plt.plot([np.prod(s) for s in sizes], times_softmax, label='F.softmax')
+plt.plot([np.prod(s) for s in sizes], times_softmax1, label='softmax1')
 plt.legend()
 plt.xlabel('Tensor Size')
 plt.ylabel('Time (s)')
-plt.title('Benchmarking Results')
+plt.title('Traditional Softmax vs SoftmaxOne')
 plt.show()
